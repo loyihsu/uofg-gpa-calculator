9094760e07f26912ee4e2ca309548036b7c26056
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-09-14 04:25:01 +0800

    Add exact value with rounding for GPA.
    According to the docs, the GPA values are rounded to .1 position.

--- src/internal/GradePointAverage.java
+++ src/internal/GradePointAverage.java
@@ -18,8 +18,28 @@ public class GradePointAverage {
         return GradeBand.getBand(grades);
     }
 
+    double getValue() {
+        double values = 0.0, gradePoints = 0.0;
+        for (Grade grade: grades) {
+            values += grade.getValue() * grade.getPercentage();
+            gradePoints += grade.getPercentage();
+        }
+
+        values /= gradePoints;
+
+        int rounder = ((int)(values * 100)) % 10;
+        values = ((double)((int)(values * 10))) / 10;
+
+        if (rounder >= 5) {
+            values += 0.1;
+        }
+
+        System.out.println(values);
+        return values;
+    }
+
     @Override
     public String toString() {
-        return "GPA: " + getGPA();
+        return "GPA: " + getGPA() + " (Exact Value: " + getValue() + ")";
     }
 }
